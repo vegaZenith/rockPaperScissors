f5bb255eca1869e5c79c3b8383218ea1d1d1120d
Dan Lehman <5494894+vegaZenith@users.noreply.github.com>
2024-08-23 10:58:00 -0400

    add function to generate a random game choice Added a function that returns rock paper or scissors randomly as the computer's guess.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+function getComputerChoice(){
+    let randomInteger = Math.floor(Math.random() * 3);
+
+    if(randomInteger === 0){
+        return "rock";
+    } else if(randomInteger === 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
