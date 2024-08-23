7255ef77fe0006c1d1242894e8cba5a3f37acf54
Dan Lehman <5494894+vegaZenith@users.noreply.github.com>
2024-08-23 11:30:51 -0400

    add a function that simulates a single round Added a function that is passed a human choice and a computer choice and compares them to determine the winner and increment the winners score.

--- main.js
+++ main.js
@@ -14,11 +14,25 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let choice = prompt("rock, paper, or scissors?");
+    let choice = prompt("rock, paper, or scissors?").toLowerCase();
     if(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
         console.log("That's not an option!");
         return "";
     } else {
         return choice;
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log("Tie");
+    } else if((humanChoice === "rock" && computerChoice === "scissors") 
+            || (humanChoice === "paper" && computerChoice === "rock") 
+            || humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("Human Wins");
+        humanScore++;
+    } else {
+        console.log("Computer Wins");
+        computerScore++;
     }
 }
